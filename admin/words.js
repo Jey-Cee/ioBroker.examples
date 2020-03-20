9a46d8ad9d4c37a5f1d403a9b13d92dd292d0054
jey-cee <jey-cee@live.com>
2020-03-20 19:51:31 +0100

    initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'examples adapter settings': {
+		'en': 'Adapter settings for examples',
+		'de': 'Adaptereinstellungen für examples',
+		'ru': 'Настройки адаптера для examples',
+		'pt': 'Configurações do adaptador para examples',
+		'nl': 'Adapterinstellingen voor examples',
+		'fr': "Paramètres d'adaptateur pour examples",
+		'it': "Impostazioni dell'adattatore per examples",
+		'es': 'Ajustes del adaptador para examples',
+		'pl': 'Ustawienia adaptera dla examples',
+		'zh-cn': 'examples的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'option2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
