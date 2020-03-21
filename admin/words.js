eeb2df9147fe7cb0e78be0f5dcb2022ecbbc8111
jey-cee <jey-cee@live.com>
2020-03-21 17:21:20 +0100

    add some content to index_m.html

--- admin/words.js
+++ admin/words.js
@@ -14,17 +14,17 @@ systemDictionary = {
 		'pl': 'Ustawienia adaptera dla examples',
 		'zh-cn': 'examples的适配器设置'
 	},
-	'option1': {
-		'en': 'option1',
-		'de': 'Option 1',
-		'ru': 'Опция 1',
-		'pt': 'Opção 1',
-		'nl': 'Optie 1',
-		'fr': 'Option 1',
-		'it': 'opzione 1',
-		'es': 'Opción 1',
-		'pl': 'opcja 1',
-		'zh-cn': '选项1'
+	'Sets / represents true or false for cb-option-1 in adapter config': {
+		'en': 'Sets / represents true or false for cb-option-1 in adapter config',
+		'de': 'Setzt / repräsentiert wahr oder falsch für cb-Option-1 in der Adapterkonfiguration',
+		'ru': 'Устанавливает / представляет истину или ложь для cb-option-1 в конфигурации адаптера',
+		'pt': 'Define / representa true ou false para cb-option-1 na configuração do adaptador',
+		'nl': 'Stelt / vertegenwoordigt waar of onwaar in voor cb-option-1 in adapterconfiguratie',
+		'fr': "Définit / représente vrai ou faux pour l'option cb-1 dans la configuration de l'adaptateur",
+		'it': "Imposta / rappresenta vero o falso per cb-option-1 nella configurazione dell'adattatore",
+		'es': 'Establece / representa verdadero o falso para cb-option-1 en la configuración del adaptador',
+		'pl': 'Ustawia / reprezentuje wartość prawda lub fałsz dla cb-opcja-1 w konfiguracji adaptera',
+		'zh-cn': '在适配器配置中为cb-option-1设置/表示是或否'
 	},
 	'option2': {
 		'en': 'option2',
